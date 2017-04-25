3242b05ff978fd0b2907b8f40a861c1635beb535
Robbie Jaeger <jaeger.rob@gmail.com>
2017-04-25 07:51:51 -0600

    Add knex init and update readme with setup

---
+++ knexfile.js
@@ -0,0 +1,32 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/knex_has_many_test',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/knex_has_many',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/production'
+    }
+  }
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 # Knex Relationships
 
 Experiments with ExpressJS backend API and Knex one-to-many and many-to-many relationships. The premise of this app is blog posts with comments (one-to-many) and categories (many-to-many).
+
+## Setup
+
+1. Clone this repo and `cd` into the new directory.
+2. In the terminal, run `npm install`.
+3. Add `knex_has_many` and `knex_has_many_test` databases to postgres.
+
+## Run Tests
+
+In the terminal, run `mocha`. (You must have Mocha installed globally for this command to work.)
+
+## Run App
+
+In the terminal, run `node server.js` and head over to `localhost:3000`.
